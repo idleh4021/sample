6aaeaa7de838f01947e532ed5103a8771dbeea97
이동현 <idleh4021@naver.com>
2022-07-15 15:40:41 +0900

    프런트엔드 html 바인딩

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,17 @@
 "use strict";
 
-console.log("hello ??");
-console.log("cool");
+const id = document.querySelector("#id");
+const pwd = document.querySelector("#pwd");
+const btn = document.querySelector("button");
+
+
+btn.addEventListener("click",login);
+
+function login(){
+    const req = {
+        id : id.value,
+        pwd : pwd.value,
+    }
+
+    console.log(req);
+}
